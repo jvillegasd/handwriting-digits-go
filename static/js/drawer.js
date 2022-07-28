d438caf718634a6e6838dde3cfa84d3aaf22a3bd
= <=>
2022-07-28 08:28:10 -0500

    feat: :sparkles: Clean canvas button css added; Clean cavas js functionality added

--- static/js/drawer.js
+++ static/js/drawer.js
@@ -4,13 +4,8 @@ const LINE_CAP = 'round';
 const STROKE_STYLE = 'black';
 const mouseCoords = { x: 0, y: 0 };
 const drawCanvas = document.getElementById('draw_canvas');
+const clearCanvasBtn = document.getElementById('clean_btn');
 const ctx = drawCanvas.getContext('2d');
-
-window.addEventListener('load', () => {
-  document.addEventListener('mousedown', startDrawing);
-  document.addEventListener('mouseup', stopDrawing);
-  document.addEventListener('mousemove', drawAtCanvas);
-});
 
 let getPosition = (event) => {
   mouseCoords.x = event.clientX - drawCanvas.offsetLeft;
@@ -38,3 +33,16 @@ let drawAtCanvas = (event) => {
   ctx.lineTo(mouseCoords.x, mouseCoords.y);
   ctx.stroke();
 };
+
+let cleanCanvas = () => {
+  ctx.beginPath();
+  ctx.clearRect(0, 0, drawCanvas.width, drawCanvas.height);
+};
+
+window.addEventListener('load', () => {
+  document.addEventListener('mousedown', startDrawing);
+  document.addEventListener('mouseup', stopDrawing);
+  document.addEventListener('mousemove', drawAtCanvas);
+});
+
+clearCanvasBtn.addEventListener('click', cleanCanvas);
